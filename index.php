2da37ff7b6c1f57f95749470f495329fc95bb918
webnetkz <8gg001@gmail.com>
2019-02-11 19:23:52 +0600

    navik

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ru">
+    <head>
+        <title>Resume</title>
+
+        <meta charset="UTF-8">
+        <meta name="author" content="TOO WebNet">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, follow">
+
+        <link rel="shortcut icon" href="miniLogoWebnet.png" type="image/png">
+        <link rel="stylesheet" href="public/style.css">
+          
+    </head>
+
+    <div id="content">
+        <h1>Резюме</h1>
+            <hr>
+        <h2>Контакты:</h2>
+
+            <p>Поздняков Ярослав Викторович</p>
+            <p>тел.: +7(707) 669 14 24,<br> почта: dbqqbq@gmail.com</p>
+        <hr>
+        <h2>Образование:</h2>
+
+            <p>Средне специальное: Академия юриспруденции — Высшая школа права "Адилет", 2008-2012</p>
+            <p>Факультет юриспруденции</p>
+        <hr>
+        <h2>Опыт:</h2>
+
+            <h3>TOO "Top Of Technology", 2014-2018</h3>
+            <h4>Монтажник систем кондиционирования, 2014-2016</h4>
+                
+                <p>Монтаж split, multi-V и multi-FDX — систем кондиционирования.</p>
+            <h4>Сервис-инженер, 2016-2018</h4>
+                
+                <p>Ремонт и профилактика систем кондиционирования.</p>
+                <p>Проектирование установки систем кондиционирования.</p>
+        <hr>
+        <h2>Навыки:</h2>
+            <p>Системное адменистрирование UNIX (LINUX, BSD) подобных операционных систем.</p>
+            <p>Профилактика и ремонт ПК и серверов.</p>
+            <p>Адменистрирование веб-серверов Apache2, NGINX.</p>
+            <p>Администрирование баз-данных MySQL, PostgreSQL.</p>
+            <p>Языки программирования: JavaScript, PHP7++, применение ООП.</p>
+            <p>Фреймворки: jQuery, React, Larevel.</p>
+            <p>Владение HTML5 и CSS4 стандартами.</p>
+            <p>SEO оптимизация под google, yandex поисковики.</p>
+            <p>Таргетированая-реклама для Instagramm, FaceBoock.</p>
+            <p>Контекстная-реклама для google, yadex.</p>
+            <p></p>
+    </div>

d84f29abc0b50cbb6a31a1f348211554d7078dae
Mac <mac@MacBook-Air-mac.local>
2024-08-14 10:52:17 +0500

    Root commit

---
+++ sw.js
@@ -0,0 +1,29 @@
+importScripts('./cache-polyfill.js');
+
+self.addEventListener('install', function(e)
+{
+ e.waitUntil(
+   caches.open('title').then(function(cache)
+   {
+     return cache.addAll([
+       //'./public/styles/style.css',
+     ]);
+   })
+ );
+});
+
+self.addEventListener('install', function(event) {
+});
+
+self.addEventListener('activate', function(event) {
+});
+
+self.addEventListener('fetch', function(event)
+{
+  event.respondWith(
+    caches.match(event.request).then(function(response)
+    {
+      return response || fetch(event.request);
+    })
+  );
+});
